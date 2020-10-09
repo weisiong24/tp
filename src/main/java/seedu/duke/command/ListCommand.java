e6379c39b109cb1d360ac935d06f9397dba966a1
manuelmanuntag96 <MANUELMANUNTAG15@GMAIL.COM>
2020-10-09 21:03:12 +0800

    Commented out ListCommand.java

--- src/main/java/seedu/duke/command/ListCommand.java
+++ src/main/java/seedu/duke/command/ListCommand.java
@@ -1,20 +1,20 @@
-package seedu.duke.command;
+//package seedu.duke.command;
 
 //import seedu.duke.storage.Storage;
-import seedu.duke.task.TaskList;
-import seedu.duke.ui.Ui;
+//import seedu.duke.task.TaskList;
+//import seedu.duke.ui.Ui;
 
 /**
  * Prints a list of all tasks to the user.
  */
-public class ListCommand extends Command {
+//public class ListCommand extends Command {
 
-    public ListCommand() {
-        super(null);
-    }
+    //public ListCommand() {
+        //super(null);
+    //}
 
-    @Override
-    public void execute(TaskList tasks, Ui ui/*, Storage storage*/) {
-        ui.printList(tasks);
-    }
-}
+    //@Override
+    //public void execute(TaskList tasks, Ui ui/*, Storage storage*/) {
+        //ui.printList(tasks);
+    //}
+//}
