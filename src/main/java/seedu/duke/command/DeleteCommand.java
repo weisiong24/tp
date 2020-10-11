fe7edcd839f49d34bd1b60256ea4f5c723913972
tammykoh <tammykohrw@gmail.com>
2020-10-12 02:28:43 +0800

    Delete function implemented User can delete a class from a day in the timetable

--- src/main/java/seedu/duke/command/DeleteCommand.java
+++ src/main/java/seedu/duke/command/DeleteCommand.java
@@ -1,4 +1,4 @@
-//package seedu.duke.command;
+package seedu.duke.command;
 
 //import seedu.duke.exception.DukeException;
 //import seedu.duke.storage.Storage;
@@ -6,21 +6,74 @@
 //import seedu.duke.task.TaskList;
 //import seedu.duke.ui.Ui;
 
+import seedu.duke.exception.DukeException;
+import seedu.duke.task.Event;
+import seedu.duke.ui.Ui;
+import seedu.duke.user.User;
+import seedu.duke.user.UserList;
+
+import java.util.ArrayList;
+
 /**
  * Deletes a task from the task list.
  */
-//public class DeleteCommand extends Command {
+public class DeleteCommand extends Command {
 
-    //public DeleteCommand(String information) {
-        //super(information);
-    //}
+    public DeleteCommand(String information) {
+        super(information);
+    }
 
-    //@Override
-    //public void execute(TaskList tasks, Ui ui/*, Storage storage*/) throws DukeException {
-        //int index = Integer.parseInt(input);
-        //Task removedTask = tasks.getTask(index);
-        //tasks.deleteTask(index);
-        //ui.printDelete(tasks, removedTask);
-        //storage.write(tasks);
-    //}
-//}
+    @Override
+    public void execute(UserList users, Ui ui, User nowUser/*, Storage storage*/) throws DukeException {
+        if (nowUser == null) {
+            throw new DukeException("Sorry! You are not logged in to any account :-(");
+        }
+
+        String[] parsedInputs = input.split("/", 3);
+        String day = parsedInputs[1].trim();
+        System.out.println("day: " + day);
+        int index;
+
+        try {
+            index = Integer.parseInt(parsedInputs[2]);
+            for (int i = 0; i < users.getTotalUserCount(); i++) {
+                if ((users.getUser(i + 1).getName() == nowUser.getName())) {
+                    ArrayList<Object> timetable = null;
+                    switch (day) {
+                    case "mon":
+                        timetable = (users.getUser(i + 1).getTimetable()).getMonTimetable();
+                        break;
+                    case "tue":
+                        timetable = (users.getUser(i + 1).getTimetable()).getTueTimetable();
+                        break;
+                    case "wed":
+                        timetable = (users.getUser(i + 1).getTimetable()).getWedTimetable();
+                        break;
+                    case "thu":
+                        timetable = (users.getUser(i + 1).getTimetable()).getThuTimetable();
+                        break;
+                    case "fri":
+                        timetable = (users.getUser(i + 1).getTimetable()).getFriTimetable();
+                        break;
+                    case "sat":
+                        timetable = (users.getUser(i + 1).getTimetable()).getSatTimetable();
+                        break;
+                    case "sun":
+                        timetable = (users.getUser(i + 1).getTimetable()).getSunTimetable();
+                        break;
+                    default:
+                        throw new DukeException("Sorry! I don't know what day you mean :-(");
+                    }
+                    String removedClass = timetable.get(index - 1).toString();
+                    timetable.remove(index - 1);
+                    ui.printDelete(removedClass, day, timetable.size());
+                    break;
+                }
+            }
+        } catch (NumberFormatException e) {
+            throw new DukeException("You've entered an invalid index!");
+        } catch (IndexOutOfBoundsException e) {
+            throw new DukeException("You've entered an invalid index!");
+        }
+    }
+}
