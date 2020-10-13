41e4c81b4645e7dcd1ad3869058ae795329cd31d
yellow-fellow <huangshaohang.98@gmail.com>
2020-10-13 23:20:04 +0800

    Added CompareCommand.java

--- src/main/java/seedu/duke/ui/Ui.java
+++ src/main/java/seedu/duke/ui/Ui.java
@@ -1,6 +1,5 @@
 package seedu.duke.ui;
 
-//import seedu.duke.task.Deadline;
 import seedu.duke.exception.DukeException;
 import seedu.duke.task.Event;
 import seedu.duke.task.Task;
@@ -179,5 +178,15 @@ public class Ui {
      */
     public void showError(String message) {
         System.out.println(MESSAGE_SAD_FACE + " OOPS!!! " + message);
+    }
+
+    /**
+     * Prints the common available timeslots between 2 timetables
+     */
+    public void printCompare(ArrayList<Integer> outputArray) {
+        System.out.println("Your common timeslots are: \n");
+        for (int availableTime : outputArray){
+            System.out.println(availableTime + "HR ");
+        }
     }
 }
