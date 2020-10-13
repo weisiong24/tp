d8ca128bdb5af9930752d0286f07422969b09da4
manuelmanuntag96 <MANUELMANUNTAG15@GMAIL.COM>
2020-10-13 08:58:31 +0800

    Passed Gradle CheckStyle

--- src/main/java/seedu/duke/command/LogInCommand.java
+++ src/main/java/seedu/duke/command/LogInCommand.java
@@ -22,7 +22,7 @@ public class LogInCommand extends Command {
         boolean doesExist = false;
         
         String[] parsedInputs = input.split(" /", 2);
-        for (int i = 1; i<users.getTotalUserCount(); i++) {
+        for (int i = 1; i < users.getTotalUserCount(); i++) {
             if (parsedInputs[0].equals((users.getUser(i)).getName())) {
                 if (parsedInputs[1].equals((users.getUser(i)).getPassWord())) {
                     currentUser = users.getUser(i);
@@ -34,7 +34,7 @@ public class LogInCommand extends Command {
             }
         } 
         
-        if (doesExist == false){
+        if (doesExist == false) {
             //System.out.println("User: " + parsedInputs[0] + " does not exist!");
             User newUser = new User(parsedInputs[0], parsedInputs[1]);
             currentUser = newUser;
