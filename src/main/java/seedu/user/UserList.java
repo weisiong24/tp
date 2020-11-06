0b43bbf8adb9a9219c2747b9f77b4f837dde760d
manuelmanuntag96 <MANUELMANUNTAG15@GMAIL.COM>
2020-11-06 19:26:29 +0800

    Edited Remove User Command to new format: remove /username /password

--- src/main/java/seedu/user/UserList.java
+++ src/main/java/seedu/user/UserList.java
@@ -46,7 +46,7 @@ public class UserList {
         return null;
     }
     
-    public void removeUser(String userName)  throws WhereGotTimeException {
+    public void removeUser(String userName, String passWord)  throws WhereGotTimeException {
         boolean isMatch = false;
         
         if (users.isEmpty()) {
@@ -56,9 +56,13 @@ public class UserList {
         for (int i = 0; i < users.size(); i++) {
             System.out.println(users.get(i).getName());
             if (users.get(i).getName().equals(userName)) {
-                users.remove(i);
-                System.out.println(userName + " has been removed.");
-                isMatch = true;
+                if (users.get(i).getPassword().equals(passWord)) {
+                    users.remove(i);
+                    System.out.println(userName + " has been removed.");
+                    isMatch = true;
+                } else {
+                    throw new WhereGotTimeException("Wrong Password");
+                }
             }
         }
 
