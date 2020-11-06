d597d4215c7535249effbe147ec7771c7ecf8011
SKYLER\User <celester989@gmail.com>
2020-11-06 18:44:10 +0800

    Updated Userguide and add more Junit testing

--- src/test/java/seedu/command/AddCommandTest.java
+++ src/test/java/seedu/command/AddCommandTest.java
@@ -3,6 +3,7 @@ package seedu.command;
 import org.junit.jupiter.api.Test;
 import seedu.task.Event;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class AddCommandTest {
 
@@ -10,12 +11,19 @@ class AddCommandTest {
     String location = "LT23";
     String timeStart = "1600";
     String timeEnd = "1900";
-    
+    String locationTest = "/LT23";
     @Test
     void testAddCommand() {
         Event e = new Event(name, location,timeStart,timeEnd);
 
         assertEquals("CS2040C LT23 1600-1900",e.toString());
+    }
+    
+    @Test
+    void testLocation() {
+        Event e1 = new Event(name, locationTest,timeStart,timeEnd);
+
+        assertNotEquals(e1.toString(),"CS2040C LT23 1600-1900");
     }
 
 }
