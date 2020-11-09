7dba8602746794adf7570c94a4a09c7e22b78202
tammykoh <tammykohrw@gmail.com>
2020-11-09 21:19:34 +0800

    Added logging for list, clear, find and delete functions

--- src/main/java/seedu/command/DeleteCommand.java
+++ src/main/java/seedu/command/DeleteCommand.java
@@ -7,7 +7,9 @@ import seedu.ui.Ui;
 import seedu.user.User;
 import seedu.user.UserList;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.logging.*;
 
 /**
  * Deletes a task from the task list.
@@ -18,9 +20,16 @@ public class DeleteCommand extends Command {
         super(information);
     }
 
+    private static Logger logger = Logger.getLogger("LogDeleteCommand");
+
     @Override
     public void execute(UserList users, Ui ui, User nowUser) throws WhereGotTimeException {
+
+        setupInputLogger();
+        logger.log(Level.INFO, "Beginning DeleteCommand...");
+
         if (nowUser == null) {
+            logger.log(Level.WARNING, "User not logged in, ending DeleteCommand function");
             throw new NotLoggedInException("Sorry! You are not logged in to any account :-(");
         }
         
@@ -29,19 +38,37 @@ public class DeleteCommand extends Command {
             String day = parsedInputs[1].trim().toLowerCase();
             int index;
             index = Integer.parseInt(parsedInputs[2]);
+            logger.log(Level.INFO, "Deleting item " + index + ", on " + day);
             for (int i = 0; i < users.getTotalUserCount(); i++) {
                 if ((users.getUser(i + 1).getName().equals(nowUser.getName()))) {
-                    ArrayList<Event> timetable = (users.getUser(i + 1).getTimetable()).getTimetable(day);;
+                    ArrayList<Event> timetable = (users.getUser(i + 1).getTimetable()).getTimetable(day);
                     String removedClass = timetable.get(index - 1).toString();
                     timetable.remove(index - 1);
                     ui.printDelete(removedClass, day, timetable.size());
+                    logger.log(Level.INFO, "Lesson has been deleted successfully");
                     break;
                 }
             }
         } catch (NumberFormatException e) {
+            logger.log(Level.WARNING, "Index entered is not an integer, ending session...");
             throw new WhereGotTimeException("You've entered an invalid index!");
         } catch (IndexOutOfBoundsException e) {
+            logger.log(Level.WARNING, "No such index in timetable, ending session...");
             throw new WhereGotTimeException("You've entered an invalid index!");
+        }
+        logger.log(Level.INFO, "DeleteCommand function has ended successfully");
+    }
+
+    private void setupInputLogger() {
+        LogManager.getLogManager().reset();
+        logger.setLevel(Level.ALL);
+        try {
+            FileHandler fh = new FileHandler("DeleteCommand.log", true);
+            fh.setLevel(Level.INFO);
+            fh.setFormatter(new SimpleFormatter());
+            logger.addHandler(fh);
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "File logging is not functional");
         }
     }
 }
--- src/main/java/seedu/command/ListCommand.java
+++ src/main/java/seedu/command/ListCommand.java
@@ -6,6 +6,9 @@ import seedu.ui.Ui;
 import seedu.user.User;
 import seedu.user.UserList;
 
+import java.io.IOException;
+import java.util.logging.*;
+
 
 /**
  * Prints a list of all tasks to the user.
@@ -16,22 +19,34 @@ public class ListCommand extends Command {
         super(input);
     }
 
+    private static Logger logger = Logger.getLogger("LogListCommand");
+
     @Override
     public void execute(UserList users, Ui ui, User nowUser) throws WhereGotTimeException {
+
+        setupInputLogger();
+        logger.log(Level.INFO, "Beginning ListCommand...");
+
         int userIndex = -1;
 
         if (nowUser == null) {
+            logger.log(Level.WARNING, "User not logged in, ending ListCommand function");
             throw new NotLoggedInException("Sorry! You are not logged in to any account!");
         }
 
         for (int i = 0; i < users.getTotalUserCount(); i++) {
             if ((users.getUser(i + 1).getName().equals(nowUser.getName()))) {
                 userIndex = i + 1;
+                logger.log(Level.INFO, "User has been found, user index: " + userIndex);
+                break;
             }
         }
 
+        assert userIndex != -1 : "User not found";
+
         String day = input.substring(1).toLowerCase();
         if (day.equals("all")) {
+            logger.log(Level.INFO, "List all classes");
             ui.printList(users, userIndex, "mon");
             System.out.println();
             ui.printList(users, userIndex, "tue");
@@ -46,7 +61,22 @@ public class ListCommand extends Command {
             System.out.println();
             ui.printList(users, userIndex, "sun");
         } else {
+            logger.log(Level.INFO, "List classes only on " + day);
             ui.printList(users, userIndex, day);
+        }
+        logger.log(Level.INFO, "ListCommand function has ended successfully");
+    }
+
+    private void setupInputLogger() {
+        LogManager.getLogManager().reset();
+        logger.setLevel(Level.ALL);
+        try {
+            FileHandler fh = new FileHandler("ListCommand.log", true);
+            fh.setLevel(Level.INFO);
+            fh.setFormatter(new SimpleFormatter());
+            logger.addHandler(fh);
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "File logging is not functional");
         }
     }
 }
